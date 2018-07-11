dc446db95f4d5ec20a9e24b740b4a9203f5b38cc
Gloria H <silverdragonia@gmail.com>
2018-07-11 16:51:11 -0400

    edits

--- js/index.js
+++ js/index.js
@@ -15,17 +15,16 @@ $(document).ready(function(){
   // project preview and screen recording
   //
   // pause the video when the modal is closed
-  $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+  $(document).on('click', '.hanging-close', '.modal-backdrop, .modal', function (event) {
     // remove the src so the player itself gets removed, as this is 
     // the only reliable way to ensure the video stops playing in IE
-    $("#screen-recording-video").empty();
+    $('#screen-recording-video').empty();
   });
   //start playing the video whenever the trailer modal is opened
-  $(document).on('click', '.video', function (event) {
+  $(document).on('click', '.video-tile', function (event) {
     var screenRecording = $(this).attr('screen-recording-id')
-    var sourceUrl = './img/screen_recording/' + screenRecording
-    + '?autoplay=1&html5=1';
-    $("#screen-recording-video").empty().append($("<iframe></iframe>", {
+    var sourceUrl = './img/screen_recording/' + screenRecording + '?autoplay=1&html5=1';
+    $('#screen-recording-video').empty().append($('<iframe></iframe>', {
       'id': 'screen-recording',
       'class': 'screen-recording',
       'width': '900px',
