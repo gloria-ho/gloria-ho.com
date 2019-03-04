92d41d8aa32c369c9c377d2f456ece9f77f24adb
Gloria H <silverdragonia@gmail.com>
2019-03-03 21:40:54 -0500

    updating layout

--- js/index.js
+++ js/index.js
@@ -1,38 +1,39 @@
-$(document).ready(function(){
-
+document.addEventListener("DOMContentLoaded", function(event) { 
+  
   // animated nav icon
-  $('.animated-nav-icon').click(function(){
-      $(this).toggleClass('open');
-  });
-
-  // animate in the videos when the page loads
-  $('.video-tile').hide().show('fast', function showNext() {
-    $(this).next('div').show('fast', showNext);
+  document.querySelector('.animated-nav-icon').click(function(){
+    this.querySelector('open') ? this.classList.remove('open') | this.classList.add('open');
   });
 
 
-  //
-  // project preview and screen recording
-  //
-  // pause the video when the modal is closed
-  $(document).on('click', '.hanging-close', '.modal-backdrop, .modal', function (event) {
-    // remove the src so the player itself gets removed, as this is 
-    // the only reliable way to ensure the video stops playing in IE
-    $('#screen-recording-video').empty();
-  });
-  //start playing the video whenever the trailer modal is opened
-  $(document).on('click', '.video-tile', function (event) {
-    var screenRecording = $(this).attr('screen-recording-id')
-    var sourceUrl = './img/screen_recording/' + screenRecording + '?autoplay=1&html5=1';
-    $('#screen-recording-video').empty().append($('<iframe></iframe>', {
-      'id': 'screen-recording',
-      'class': 'screen-recording',
-      'width': '900px',
-      'type': 'text-html',
-      'src': sourceUrl,
-      'frameborder': 0
-    }));
-  });
+  // // animate in the videos when the page loads
+  // $('.video-tile').hide().show('fast', function showNext() {
+  //   $(this).next('div').show('fast', showNext);
+  // });
+
+
+  // //
+  // // project preview and screen recording
+  // //
+  // // pause the video when the modal is closed
+  // $(document).on('click', '.hanging-close', '.modal-backdrop, .modal', function (event) {
+  //   // remove the src so the player itself gets removed, as this is 
+  //   // the only reliable way to ensure the video stops playing in IE
+  //   $('#screen-recording-video').empty();
+  // });
+  // //start playing the video whenever the trailer modal is opened
+  // $(document).on('click', '.video-tile', function (event) {
+  //   var screenRecording = $(this).attr('screen-recording-id')
+  //   var sourceUrl = './img/screen_recording/' + screenRecording + '?autoplay=1&html5=1';
+  //   $('#screen-recording-video').empty().append($('<iframe></iframe>', {
+  //     'id': 'screen-recording',
+  //     'class': 'screen-recording',
+  //     'width': '900px',
+  //     'type': 'text-html',
+  //     'src': sourceUrl,
+  //     'frameborder': 0
+  //   }));
+  // });
 
 
 })
